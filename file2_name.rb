99ce971d351f56d6df3fd1b34c5a3c99defed97b
Nicholas Wachira <100451980+Tocopro@users.noreply.github.com>
2022-05-13 11:16:32 -0500

    Create file2_name.rb

---
+++ file2_name.rb
@@ -0,0 +1,10 @@
+
+# frozen_string_literal: true
+
+def check_numbers(one, two, three)
+  (one >= 1 && one <= 10)
+  (two >= 1 && two <= 10)
+  (three >= 1 && three <= 10)
+end
+check = check_numbers(3, 5, 60)
+puts check
